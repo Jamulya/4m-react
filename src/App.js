ae7922b980c7084c6ef42e78370b30384f45e951
jamulya <jamka_kg@mail.ru>
2022-11-09 20:01:38 +0600

    lesson1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header/Header";
+import './components/style.css'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <main>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/FirstBlock/FirstBlock.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FirstBlock = () => {
+  return (
+    <section className=''>
+        <div className=''>
+
+        </div>
+    </section>
+  )
+}
+
+export default FirstBlock;
